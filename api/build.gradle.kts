5fa987152183ba7145099c50c7b598bb170a3846
Avyvka <lamonov.yura@gmail.com>
2025-09-25 00:54:49 +0300

    Update mapstruct to 1.6.3

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -25,13 +25,13 @@ repositories {
 
 dependencies {
     implementation("org.liquibase:liquibase-core")
-    implementation("org.mapstruct:mapstruct:1.6.2")
+    implementation("org.mapstruct:mapstruct:1.6.3")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework:spring-jdbc")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("org.postgresql:r2dbc-postgresql")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.2")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
