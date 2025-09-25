f3d92373ec493c73e3f4f4da4ee624f2eb3fdc9e
Avyvka <lamonov.yura@gmail.com>
2025-09-25 20:41:40 +0300

    Fix repositoryBaseClass

--- api/src/main/java/com/github/avyvka/game/library/GameLibraryApplication.java
+++ api/src/main/java/com/github/avyvka/game/library/GameLibraryApplication.java
@@ -1,9 +1,12 @@
 package com.github.avyvka.game.library;
 
+import com.github.avyvka.game.library.repository.support.CustomSimpleR2dbcRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories(repositoryBaseClass = CustomSimpleR2dbcRepository.class)
 public class GameLibraryApplication {
 
 	public static void main(String[] args) {
