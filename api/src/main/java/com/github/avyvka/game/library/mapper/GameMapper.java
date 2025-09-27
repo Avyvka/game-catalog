dfd4c8db0e660643f920bde11837e3ad158014f5
Avyvka <lamonov.yura@gmail.com>
2025-09-27 22:08:06 +0300

    Add init tables changelog

--- api/src/main/java/com/github/avyvka/game/library/mapper/GameMapper.java
+++ api/src/main/java/com/github/avyvka/game/library/mapper/GameMapper.java
@@ -1,6 +1,7 @@
 package com.github.avyvka.game.library.mapper;
 
 import com.github.avyvka.game.library.mapper.api.EntityDtoMapper;
+import com.github.avyvka.game.library.model.dto.GameDto;
 import com.github.avyvka.game.library.model.entity.GameEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
@@ -10,5 +11,5 @@ import org.mapstruct.ReportingPolicy;
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
-public interface GameMapper extends EntityDtoMapper<GameEntity, GameEntity> {
+public interface GameMapper extends EntityDtoMapper<GameEntity, GameDto> {
 }
