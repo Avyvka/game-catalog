4cf113c85dcdebed4d5ab5fcb1b2071f74bfd861
Avyvka <lamonov.yura@gmail.com>
2025-09-28 13:39:58 +0300

    Hide null values in dto

--- api/src/main/java/com/github/avyvka/game/library/model/dto/GenreDto.java
+++ api/src/main/java/com/github/avyvka/game/library/model/dto/GenreDto.java
@@ -1,10 +1,12 @@
 package com.github.avyvka.game.library.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record GenreDto(
         UUID id,
         @NotBlank @Size(max = 32) String name
