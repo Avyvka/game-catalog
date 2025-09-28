8f5f7de65c0324b82b14c9b57bc1469dea4c6b2c
Avyvka <lamonov.yura@gmail.com>
2025-09-28 20:02:44 +0300

    Service repackages

--- api/src/main/java/com/github/avyvka/game/library/controller/DeveloperReactiveCrudController.java
+++ api/src/main/java/com/github/avyvka/game/library/controller/DeveloperReactiveCrudController.java
@@ -2,7 +2,7 @@ package com.github.avyvka.game.library.controller;
 
 import com.github.avyvka.game.library.controller.support.AbstractReactiveCrudController;
 import com.github.avyvka.game.library.model.dto.DeveloperDto;
-import com.github.avyvka.game.library.service.api.ReactiveCrudService;
+import com.github.avyvka.game.library.service.DeveloperService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ import java.util.UUID;
 @RequestMapping("/api/v1/developers")
 public class DeveloperReactiveCrudController extends AbstractReactiveCrudController<DeveloperDto, UUID> {
 
-    protected DeveloperReactiveCrudController(ReactiveCrudService<DeveloperDto, UUID> service) {
+    protected DeveloperReactiveCrudController(DeveloperService service) {
         super(service);
     }
 }
--- api/src/main/java/com/github/avyvka/game/library/controller/GameReactiveCrudController.java
+++ api/src/main/java/com/github/avyvka/game/library/controller/GameReactiveCrudController.java
@@ -2,7 +2,7 @@ package com.github.avyvka.game.library.controller;
 
 import com.github.avyvka.game.library.controller.support.AbstractReactiveCrudController;
 import com.github.avyvka.game.library.model.dto.GameDto;
-import com.github.avyvka.game.library.service.api.ReactiveCrudService;
+import com.github.avyvka.game.library.service.GameService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ import java.util.UUID;
 @RequestMapping("/api/v1/games")
 public class GameReactiveCrudController extends AbstractReactiveCrudController<GameDto, UUID> {
 
-    protected GameReactiveCrudController(ReactiveCrudService<GameDto, UUID> service) {
+    protected GameReactiveCrudController(GameService service) {
         super(service);
     }
 }
