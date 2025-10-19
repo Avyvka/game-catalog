a906fde05497cd91285ff2d3fd986db8dab01739
Avyvka <lamonov.yura@gmail.com>
2025-10-20 02:51:47 +0300

    Fix redirects

--- frontend/src/app/dashboard/route.ts
+++ frontend/src/app/dashboard/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export function GET(request: Request) {
-  return NextResponse.redirect(new URL("/dashboard/games", request.url));
+export function GET() {
+  return NextResponse.redirect(new URL("/dashboard/games"));
 }
--- frontend/src/app/route.ts
+++ frontend/src/app/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
-export function GET(request: Request) {
-  return NextResponse.redirect(new URL("/dashboard", request.url));
+export function GET() {
+  return NextResponse.redirect(new URL("/dashboard"));
 }
